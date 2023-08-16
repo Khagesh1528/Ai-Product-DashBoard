9670429f204494f0d1d54463bc5772b2d26ce833
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-08-16 10:13:47 +0530

    Add files via upload

---
+++ mockAI.js
@@ -0,0 +1,7 @@
+function generatePrediction(inputData) {
+    // Replace this with your actual AI model prediction logic
+    const randomPrediction = Math.random() * 100;
+    return randomPrediction.toFixed(2);
+}
+
+module.exports = { generatePrediction };
